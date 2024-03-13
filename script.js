0cae3b8ec007489a9dd1249ca3afceff75fc7138
joaovictormtl <101139009+joaovictormtl@users.noreply.github.com>
2024-03-13 23:19:04 +0000

    Mudei a Interface

--- script.js
+++ script.js
@@ -8,97 +8,98 @@ display = getQ('#time');
 btn = getQ(".btn")
 let cardBefore = ""
 console.log(container)
-let durations = 60;  
+let durations = 60;
 
-function setime2(t){
+function setime2(t) {
   if (durations < 0 || t == 12) {
     clearInterval(decrem)
-btn.style.display = "block"
+    btn.style.display = "block"
     return
   }
-  display.innerHTML = `⌛${durations < 10 ? ("0"+durations--) : durations--}`
+  display.innerHTML = `⌛${durations < 10 ? ("0" + durations--) : durations--}`
 }
 
-function newGame(){
-  
-emojis = 
-["🤐","😅","😡","🤗","😡","🧐","🤯","😅","🧐","🤐","🤗","🤯"].sort(function(){return 0.5 - Math.random()})
-container.innerHTML = 
-emojis.map((e)=>{ 
-  return `<div class="item"><span>${e}</span></div>`
-}).toString().replaceAll(",","")
+function newGame() {
+
+  emojis =
+    ["🤐", "😅", "😡", "🤗", "😡", "🧐", "🤯", "😅", "🧐", "🤐", "🤗", "🤯"].sort(function() { return 0.5 - Math.random() })
+  container.innerHTML =
+    emojis.map((e) => {
+      return `<div class="item"><span>${e}</span></div>`
+    }).toString().replaceAll(",", "")
   addEvent()
-  durations = 60; 
-  decrem =setInterval(setime2, 1000);
+  durations = 60;
+  decrem = setInterval(setime2, 1000);
   btn.style.display = "none"
 }
 
 function addEvent() {
-spans = document.querySelectorAll('.item');
-   for (let span of spans) {   span.addEventListener("click",clickCard);
-   }
+  spans = document.querySelectorAll('.item');
+  for (let span of spans) {
+    span.addEventListener("click", clickCard);
+  }
 }
 
 function rmvEvent() {
-spans = document.querySelectorAll('.item');
-   for (let span of spans) {
-span.removeEventListener("click",clickCard);
-   }
+  spans = document.querySelectorAll('.item');
+  for (let span of spans) {
+    span.removeEventListener("click", clickCard);
+  }
 }
 
 newGame()
 btn.style.display = "none"
 
-async function clickCard(){
+async function clickCard() {
   console.log(cardBefore)
   span = this.querySelector("span")
-this.style.transition="transform 0.6s"
- this.style.transform= "rotateY(180deg)";
+  this.style.transition = "transform 0.6s"
+  this.style.transform = "rotateY(180deg)";
   console.log(span)
-  
-setTimeout(()=>{
-  span.style.display = "block"
-  span.style.transform = "rotateY(180deg)"
-},300)
-  
-  if(cardBefore == ""){
-cardBefore = this
+
+  setTimeout(() => {
+    span.style.display = "block"
+    span.style.transform = "rotateY(180deg)"
+  }, 300)
+
+  if (cardBefore == "") {
+    cardBefore = this
     return
   } else if (span.innerHTML == cardBefore.querySelector("span").innerHTML) {
-     console.log("muda");
-    this.style.border= "solid 2px tomato";
-cardBefore.style.border="solid 2px tomato"
-    cardBefore=""
+    console.log("muda");
+    this.style.border = "solid 2px tomato";
+    cardBefore.style.border = "solid 2px tomato"
+    cardBefore = ""
     chkWin()
-  }else {
-     console.log(`nao mudou`);
+  } else {
+    console.log(`nao mudou`);
     rmvEvent()
-setTimeout(()=>{
-  addStyle([this,cardBefore])
+    setTimeout(() => {
+      addStyle([this, cardBefore])
 
-  span.style.display = "none";
-cardBefore.querySelector("span").style.display = "none";
-  cardBefore = "";
-  addEvent()
-},1000)
+      span.style.display = "none";
+      cardBefore.querySelector("span").style.display = "none";
+      cardBefore = "";
+      addEvent()
+    }, 1000)
   }
 }
 
 function addStyle(elems) {
-for(let el of elems){
-   el.style.transition="transform 0.6s"
- el.style.transform= "rotateY(-180deg)";
- el.style.border= "solid 2px #333";
-}
+  for (let el of elems) {
+    el.style.transition = "transform 0.6s"
+    el.style.transform = "rotateY(-180deg)";
+    el.style.border = "solid 2px #333";
+  }
 }
 
 function chkWin() {
   let count = 1
   spans = document.querySelectorAll('.item');
   for (let span of spans) {
-if(span.querySelector("span").style.display == "block"){
-count++
-}
-   }
+    if (span.querySelector("span").style.display == "block") {
+      count++
+    }
+  }
   setime2(count)
 }
